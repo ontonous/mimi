c2eeb240d3078c3645aeb97aafb7ccd2859e46f8
ontonous <ontonous@gmail.com>
2026-07-15 19:19:11 +0800

    feat(codegen): Map of Result of List product dual
    from_json/to_json/Display for Map values Result of List of product-tuples.
    
    Invariant: L1
    Tests: dual_from_json_map_result_list_product_tuple, dual_from_json_ (85)

--- src/codegen/expr/call/method.rs
+++ src/codegen/expr/call/method.rs
@@ -2135,6 +2135,49 @@ impl<'ctx> CodeGenerator<'ctx> {
                                                 .into());
                                         }
                                     }
+                                    // Result of List of product.
+                                    if ln == "Result" && mn == "List" && margs.len() == 1 {
+                                        let list_elem = match &margs[0] {
+                                            Type::Name(an, aargs) if aargs.is_empty() => {
+                                                if let Some(td) = self.type_defs.get(an) {
+                                                    if let crate::ast::TypeDefKind::Alias(inner) =
+                                                        &td.kind
+                                                    {
+                                                        inner.clone()
+                                                    } else {
+                                                        margs[0].clone()
+                                                    }
+                                                } else {
+                                                    margs[0].clone()
+                                                }
+                                            }
+                                            other => other.clone(),
+                                        };
+                                        if let Type::Tuple(elems) = list_elem {
+                                            let arity = elems.len() as u64;
+                                            let func = self.get_runtime_fn(
+                                                "mimi_map_from_json_result_list_product_i64",
+                                            )?;
+                                            let result = self.build_call(
+                                                func,
+                                                &[
+                                                    BasicMetadataValueEnum::PointerValue(raw_ptr),
+                                                    BasicMetadataValueEnum::IntValue(
+                                                        self.context
+                                                            .i64_type()
+                                                            .const_int(arity, false),
+                                                    ),
+                                                ],
+                                                "map_from_json_result_list_product",
+                                            )?;
+                                            return Ok(self
+                                                .expect_basic_value(
+                                                    &result,
+                                                    "mimi_map_from_json_result_list_product_i64",
+                                                )?
+                                                .into());
+                                        }
+                                    }
                                     // Option of Result of product.
                                     if ln == "Option" && mn == "Result" && !margs.is_empty() {
                                         let res_ok = match &margs[0] {
